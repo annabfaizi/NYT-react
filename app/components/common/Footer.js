cdaa704507c7809963efb7963e2b2c144e4c7c15
Anna B Faizi <annanoor.b@gmail.com>
2017-09-09 11:37:55 -0700

    css changes

--- app/components/common/Footer.js
+++ app/components/common/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer className="footer">
     <hr />
-    <p>Made by Anna Faizi with lots of <i className="fa fa-heart" aria-hidden="true" /></p>
+    <p>Made by Anna B. Faizi with lots of <i className="fa fa-heart" aria-hidden="true" /></p>
   </footer>
 );
 
